87b7f8df8a84d1bd56b5723f2019b45692762224
malbert164422 <malbert16442@hotmail.com>
2017-06-20 17:00:31 -0400

    Update cos.mk

--- cos.mk
+++ cos.mk
@@ -15,30 +15,13 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from bacon device
-$(call inherit-product, device/samsung/klteduos/full_klteduos.mk)
-
-# Inherit some common CM stuff.
+# Inherit some common COS stuff.
 $(call inherit-product, vendor/cos/common.mk)
 
 COS_RELEASE=true
 PRODUCT_NAME := cos_klte
 PRODUCT_DEVICE := klte
-PRODUCT_MANUFACTURER := Samsung
-PRODUCT_MODEL := G900x
 INCLUDE_MAGISK=true
-
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
-
-PRODUCT_BRAND := samsung
-TARGET_VENDOR := samsung
-TARGET_VENDOR_PRODUCT_NAME := klte
-TARGET_VENDOR_DEVICE_NAME := G900x
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=G900x PRODUCT_NAME=klte
 
-## Use the latest approved GMS identifiers unless running a signed build
-ifneq ($(SIGN_BUILD),true)
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=samsung/klteduos/G900x:6.0.1/MHC19Q/ZNH2KAS1KN:user/release-keys \
-    PRIVATE_BUILD_DESC="klteduos-user 6.0.1 MHC19Q ZNH2KAS1KN release-keys"
-endif
+$(call inherit-product, device/samsung/klte/full_klte.mk)
